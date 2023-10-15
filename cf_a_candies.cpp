b3ef4fac8cce9f90a2c768f7c39a64916e26d348
rahat0523 <rahatbinhafiz40@gmail.com>
2023-10-15 23:07:21 +0600

    r

--- cf_a_candies.cpp
+++ cf_a_candies.cpp
@@ -7,17 +7,19 @@ int main()
    ios_base::sync_with_stdio (0);
    cin.tie (0);
    cout.tie (0);
-   ll t,a;
+   ll t;
    cin>>t;
    while(t--)
    {
-    ll n,ans;
+    ll n,ans,a;
     cin>>n;
-    while(n%a!=0)
+    a=3;
+    ans=n/3;
+    while(n%a)
     {
-        a=3;
+        a=a*2+1;
         ans=n/a;
-        a*=(a+1);
+        
     }
     cout<<ans<<endl;
    }
